20c6d4f9c8db005d0c04ba6aac5a5d94b538a48f
Heitor Henrique Gonçalves <hh.oliveira01@gmail.com>
2026-05-20 19:09:53 -0300

    feat: configurar integração inicial com json-server

---
+++ codigo/public/assets/js/tela.js
@@ -0,0 +1,6 @@
+const API_URL = "http://localhost:3000/tecnicos";
+
+const listaTecnicos = document.getElementById("lista-tecnicos");
+const perfilTecnico = document.getElementById("perfil-tecnico");
+
+const tituloAvaliacao = document.getElementById("titulo-avaliacao");
