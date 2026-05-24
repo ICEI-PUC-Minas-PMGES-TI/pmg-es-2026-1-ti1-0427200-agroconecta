8d39c1a525d5e4fcb8897d814d2f3f6545fac369
Thiago Guerra de Araujo <thiagoguerra199@gmail.com>
2026-05-23 22:03:28 -0300

    Alteracoes finais

--- codigo/public/tela-simulacao-juros/script.js
+++ codigo/public/tela-simulacao-juros/script.js
@@ -1,20 +1,34 @@
 function calcular() {
 
-let total = document.getElementById("valorTotal").value;
-let entrada = document.getElementById("entrada").value;
-let parcelas = document.getElementById("parcelas").value;
-let juros = document.getElementById("juros").value;
+    let total = document.getElementById("valorTotal").value;
+    let entrada = document.getElementById("entrada").value;
+    let parcelas = document.getElementById("parcelas").value;
+    let juros = document.getElementById("juros").value;
 
-total = Number(total);
-entrada = Number(entrada);
-parcelas = Number(parcelas);
-juros = Number(juros);
+    total = Number(total);
+    entrada = Number(entrada);
+    parcelas = Number(parcelas);
+    juros = Number(juros);
 
-let conta = total - entrada;
-conta = conta + (conta * juros / 100);
-conta = conta / parcelas;
 
-document.getElementById("resultado").innerHTML =
-"Parcelas de R$ " + conta.toFixed(2);
+    let simulacao = {
+
+        valorTotal: total,
+        valorEntrada: entrada,
+        numeroParcelas: parcelas,
+        taxaJuros: juros
+
+    };
+
+    let conta = total - entrada;
+
+    conta = conta + (conta * juros / 100);
+
+    conta = conta / parcelas;
+
+    document.getElementById("resultado").innerHTML =
+        "Parcelas de R$ " + conta.toFixed(2);
+
+    console.log(simulacao);
 
 }
