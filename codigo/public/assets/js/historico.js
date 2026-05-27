6593a180b8563a7121fcc929b26149a26bf9ceae
Heitor Henrique Gonçalves <hh.oliveira01@gmail.com>
2026-05-27 16:17:37 -0300

    feat: implementar carregamento de serviços da API

--- codigo/public/assets/js/historico.js
+++ codigo/public/assets/js/historico.js
@@ -4,3 +4,11 @@ const API_VENDAS = "http://localhost:3000/historicoVendas";
 const listaServicos = document.getElementById("lista-servicos");
 
 const listaVendas = document.getElementById("lista-vendas");
+async function carregarServicos() {
+
+  const response = await fetch(API_SERVICOS);
+
+  const servicos = await response.json();
+
+  renderizarServicos(servicos);
+}
