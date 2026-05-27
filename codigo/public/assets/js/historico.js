b926365fde77cb52d52f846dbfd4fda49ad41446
Heitor Henrique Gonçalves <hh.oliveira01@gmail.com>
2026-05-27 16:14:34 -0300

    feat: adicionar seleção de elementos do histórico

--- codigo/public/assets/js/historico.js
+++ codigo/public/assets/js/historico.js
@@ -1,3 +1,6 @@
 const API_SERVICOS = "http://localhost:3000/historicoServicos";
 
 const API_VENDAS = "http://localhost:3000/historicoVendas";
+const listaServicos = document.getElementById("lista-servicos");
+
+const listaVendas = document.getElementById("lista-vendas");
